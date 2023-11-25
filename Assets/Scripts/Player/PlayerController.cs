c23383d8c5e5f18cd1d2a878e5019c7a70845a1f
XyroDp59 <81575508+XyroDp59@users.noreply.github.com>
2023-11-25 14:08:31 +0100

    Update player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -37,8 +37,8 @@ public class PlayerController : MonoBehaviour
 	
     void Update()
     {
-
-
+		isGrounded = Physics.Raycast(transform.position, -Vector3.up * 0.05f, out hit);
+		
         // Handle player input
         float horizontalInput = Input.GetAxis("Horizontal");
 
@@ -61,12 +61,14 @@ public class PlayerController : MonoBehaviour
 			{
 				Jump(jumpForce);
 				_coyoteTimer = -1f;
+				Debug.Log("Coyote");
 			}
 			
 			else if (!isGrounded && canDoubleJump)
 			{
 				Jump(doubleJumpForce);
 				canDoubleJump = false;
+				Debug.Log("Double Jump");
 			}
 						// ou on active le timer pour le jump buffer
 			else if (!isGrounded)
@@ -81,6 +83,7 @@ public class PlayerController : MonoBehaviour
                 if (_jumpBufferTimer > 0)
                 {
                     Jump(jumpForce);
+					Debug.Log("JumpBuffer");
                 }
 
 
@@ -91,6 +94,7 @@ public class PlayerController : MonoBehaviour
                 _jumpBufferTimer = -1;
 
 				// On decremente les compteurs
+				Debug.Log("grounded");
 				if (_coyoteTimer > -1) _coyoteTimer -= Time.deltaTime;
 				if (_jumpBufferTimer > -1) _jumpBufferTimer -= Time.deltaTime;
             }
@@ -120,11 +124,16 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+		Debug.Log(collision.gameObject);
         isGrounded = true;
     }
 
-
-
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+		Debug.Log(collision.gameObject);
+        isGrounded = false;
+    }
+	
 
 
 }
