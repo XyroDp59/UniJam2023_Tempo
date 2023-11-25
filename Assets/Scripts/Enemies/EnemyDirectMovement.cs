c37a7e1a9488b1a315462a6e872b408bd188d37e
De En Jonathan WENG <de-en-jonathan.weng@telecom-sudparis.eu>
2023-11-25 20:32:26 +0100

    Added warp to link bottom of map to top of map

--- Assets/Scripts/Enemies/EnemyDirectMovement.cs
+++ Assets/Scripts/Enemies/EnemyDirectMovement.cs
@@ -5,18 +5,25 @@ namespace Enemies
     public class EnemyDirectMovement : MonoBehaviour
     {
         private Transform player;
+        private Rigidbody2D rb;
         [SerializeField] private float speed = 3.0f;
+        [SerializeField] private float maxSpeed = 5.0f;
         // Start is called before the first frame update
         void Start()
         {
             player = GameObject.Find("Player").transform;
+            rb = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            Vector3 directionToPlayer = (player.transform.position - this.transform.position).normalized;
-            transform.Translate(Time.deltaTime * speed * directionToPlayer);
+            Vector2 directionToPlayer = (player.transform.position - this.transform.position).normalized;
+            rb.velocity += Time.deltaTime * speed * directionToPlayer;
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = maxSpeed * rb.velocity.normalized;
+            }
         }
     }
 }
