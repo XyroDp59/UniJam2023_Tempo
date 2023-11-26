a2834b4a56d20159e52043ec83c2e1fbe1f395c3
Thomas-Chanu <thomas20030603@Gmail.com>
2023-11-26 15:06:57 +0100

    Spawn Extérieur

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -12,20 +12,16 @@ public class UIManager : MonoBehaviour
     public float timeRemaining;
     private float lastSecond;
 
-    public int score = 0;
-    private int scorePerSecond = 5;
 
     public TMPro.TextMeshProUGUI timerText;
-    public TMPro.TextMeshProUGUI scoreText;
-    public TMPro.TextMeshProUGUI scoreIncreaseText;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        DisplayScore();
         lastSecond = timeRemaining;
         globalTime = timeRemaining;
-        scoreIncreaseText.color = UnityEngine.Color.red;
+
 
     }
 
@@ -37,41 +33,14 @@ public class UIManager : MonoBehaviour
 
 
   
-
-
-    public void IncreaseScore(int scoreToAdd)
-    {
-        score += scoreToAdd;
-        scoreIncreaseText.text = "+" + scoreToAdd + "!";
-        scoreIncreaseText.canvasRenderer.SetAlpha(1);
-        scoreIncreaseText.transform.rotation = Quaternion.Euler(0, 0, Random.Range(-10, 10));
-
-        UnityEngine.Color currentColor = scoreIncreaseText.color;
-
-        if (currentColor == UnityEngine.Color.red)
-            scoreIncreaseText.color = UnityEngine.Color.green;
-        else if(currentColor == UnityEngine.Color.green)
-            scoreIncreaseText.color = UnityEngine.Color.blue;
-        else if(currentColor == UnityEngine.Color.blue)
-            scoreIncreaseText.color = UnityEngine.Color.red;
-
-
-        scoreIncreaseText.CrossFadeAlpha(0, 0.7f, true);
-        DisplayScore();
-    }
-
-    void DisplayScore()
-    {
-        scoreText.text = "Score\n" + score;
-    }
+    
     void UpdateTimer()
     {
         float timeToDisplay = timeRemaining + 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         if (seconds > lastSecond)
-        {
-            IncreaseScore(scorePerSecond);
+        { 
             lastSecond = seconds;
 
         }
--- Assets/Scripts/WarpManagerTop.cs
+++ Assets/Scripts/WarpManagerTop.cs
@@ -9,7 +9,7 @@ public class WarpManagerTop : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         string test = other.gameObject.tag;
-        if (test == "Player" || test == "Projectile" || test == "Enemy")
+        if (test == "Player" || test == "Projectile" || test == "Enemy" && other.GetComponent<Rigidbody2D>().velocity.y > 0)
         {
             Vector3 originalPosition = other.transform.position;
             other.transform.position = new Vector3(originalPosition.x, mapLowerBound, originalPosition.z);
