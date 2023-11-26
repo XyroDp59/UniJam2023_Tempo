d8177d6b4bf1172b488b2a34abf653a98f9f7471
Raton <149146535+Raton-mng@users.noreply.github.com>
2023-11-26 13:59:02 +0100

    game over screen

--- Assets/Scripts/UI/EndScreen.cs
+++ Assets/Scripts/UI/EndScreen.cs
@@ -1,14 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class EndScreen : MonoBehaviour
 {
-    
+    private Animator anim;
+    public GameObject parent;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(Wait(0.66f));
+        anim = GetComponent<Animator>();
+        anim.Play("GameOverAnimation");
+        parent = transform.parent.gameObject;
+    }
+
+    public void EndCredit()
+    {
+        GetComponent<Renderer>().enabled = false;
+        StartCoroutine(Wait(0.5f));
+        for (int i = 1; i <= 3; i += 1)
+        {
+           parent.transform.GetChild(i).gameObject.SetActive(true); 
+        }
+    }
+    
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
     }
 }
